54bd66aea6cba4a82a7757c3c411d5cd3cccd2a0
Shannon TJ <shannontj@shaw.ca>
2020-12-09 14:17:44 -0700

    add context

---
+++ src/context/projects-context.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from "react";
+import { useProjects } from "../hooks";
+
+export const ProjectsContext = createContext();
+export const ProjectsProvider = ({ children }) => {
+  //set projects when adding a task in different ways (sidebar, header, etc)
+  const { projects, setProjects } = useProjects();
+
+  return (
+    <ProjectsContext.Provider value={{ projects, setProjects }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export const useProjectsValue = () => useContext(ProjectsContext);
---
+++ src/context/selected-project-context.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from "react";
+import { useProjects } from "../hooks";
+
+export const SelectedProjectContext = createContext();
+export const SelectedProjectProvider = ({ children }) => {
+  //inbox is the default selected project
+  const { selectedProject, setSelectedProject } = useState("INBOX");
+
+  return (
+    <SelectedProjectContext.Provider
+      value={{ selectedProject, setSelectedProject }}
+    >
+      {children}
+    </SelectedProjectContext.Provider>
+  );
+};
+
+export const useSelectedProjectValue = () => useContext(SelectedProjectContext);
