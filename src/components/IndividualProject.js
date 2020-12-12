42a952829cd03b56459def634548b57c8f03a03d
Shannon TJ <shannontj@shaw.ca>
2020-12-11 19:02:01 -0700

    individual project component

--- src/components/IndividualProject.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
-import { useProjectsValue, useSelectedProjectValue } from "./context";
-import { firebase } from "../firebase";
-
-export const IndividualProject = ({ project }) => {};
