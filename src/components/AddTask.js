d9e1863e244bfc93f99137469ee169d1b69564b3
Shannon TJ <shannontj@shaw.ca>
2020-12-13 23:48:07 -0700

    addtask function

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,7 @@ import { FaRegListAlt, FaRegCalendarAlt } from "react-icons/fa";
 import moment from "moment";
 import { firebase } from "../firebase";
 import { useSelectedProjectValue } from "../context";
+import { collatedTasks } from "../constants";
 
 export const AddTask = ({
   showAddTaskMain = true,
@@ -18,6 +19,38 @@ export const AddTask = ({
   const [showTaskDate, setShowTaskDate] = useState(false);
 
   const { selectedProject } = useSelectedProjectValue();
+
+  const addTask = () => {
+    const projectId = project || selectedProject;
+    let collatedDate = "";
+
+    if (projectId === "TODAY") {
+      collatedDate = moment().format("DD/MM/YYYY");
+    } else if (projectId === "NEXT_7") {
+      collatedDate = moment().add(7, "days").format("DD/MM/YYYY");
+    }
+
+    return (
+      task &&
+      projectId &&
+      firebase
+        .firestore()
+        .collection("tasks")
+        .add({
+          archived: false,
+          projectId,
+          task,
+          date: collatedDate || taskDate,
+          userId: "1234",
+        })
+        .then(() => {
+          setTask("");
+          setProject("");
+          setShowMain("");
+          setShowProjectOverlay(false);
+        })
+    );
+  };
 
   return <p></p>;
 };
