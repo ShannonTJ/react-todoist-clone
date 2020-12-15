bc300bad03b000996c134c542edec15823dc0b71
Shannon TJ <shannontj@shaw.ca>
2020-12-14 19:52:14 -0700

    add darkmode toggle to header

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,13 @@ import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
   //api data
+  apiKey: "AIzaSyAGxY5go6TXNihPUkU_iier8vyzjEHyXuQ",
+  authDomain: "react-todoist-clone-7e07b.firebaseapp.com",
+  databaseURL: "https://react-todoist-clone-7e07b-default-rtdb.firebaseio.com",
+  projectId: "react-todoist-clone-7e07b",
+  storageBucket: "react-todoist-clone-7e07b.appspot.com",
+  messagingSenderId: "1030451947260",
+  appId: "1:1030451947260:web:a4f0704c963d9b89e21979",
 });
 
 export { firebaseConfig as firebase };
